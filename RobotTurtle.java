58b05639e609294637d4d3f35a27649db9b9a716
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-20 18:30:50 -0300

    Update RobotTurtle.java

--- RobotTurtle.java
+++ RobotTurtle.java
@@ -1,7 +1,13 @@
-/*Comp 3721 - Object-Oriented Design and Methodology
-* Milestone 3
-* Due October 27th, 2020
-* Marcia Gallant and Loryn Losier
-*/
+//Marcia Gallant and Loryn Losier
+private int yPos, xPos, direction;
+private String name, colour;
 
-public class RobotTurtle
+public class RobotTurtle(int x, int y, int d, String n, String c)
+{
+  xPos = x;
+  yPos = y;
+  direction = d;
+  name = n;
+  colour = c;
+  
+}
