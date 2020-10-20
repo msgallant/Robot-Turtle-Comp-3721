b732da6cf992cfdf0eb1dc05a600db0840624ad2
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-20 19:43:32 -0300

    Update GameBoardModel.java

--- GameBoardModel.java
+++ GameBoardModel.java
@@ -1,30 +1,50 @@
 public class GameBoardModel
 {
   private int numPlayers;
+  private int numCards = 3;
   private String message;
   private ArrayList<RobotTurtle> listOfTurtles = new ArrayList<RobotTurtle>();
+  private Card[] cardDeck = new Card[numCards];
   public GameBoardModel(int players)
   {
     numPlayers = players;
     
   }
   public static void createRobotTurtles()
+  { 
+    if (numPlayers >= 1)
+    {
+      //int x, int y, int d, String n, String c
+      RobotTurtle beep = new RobotTurtle(0,0,3,"Beep", "Blue");
+      listOfTurtles.add(beep);
+    }
+    if (numPlayers >= 2)
+    {
+      RobotTurtle pangle = new RobotTurtle(0,7,4,"Pangle", "Green");
+      listOfTurtles.add(pangle);
+    }
+    if (numPlayers >= 3)
+    {
+      RobotTurtle dot = new RobotTurtle(7,0,2,"Dot", "Purple");
+      listOfTurtles.add(dot);
+    }
+    if (numPlayers >= 4)
+    {
+      RobotTurtle pi = new RobotTurtle(7,7,1,"Pi", "Red");
+      listOfTurtles.add(pi);
+    }
+
+  }
+  public static void createCardDeck()
   {
-    if (numPlayers == 1)
-    {
-      listOfTurtles.add(
-    }
-    else if (numPlayers == 2)
-    {
-      listOfTurtles.add(
-    }
-    else if (numPlayers == 3)
-    {
-      listOfTurtles.add(
-    }
-    else
-    {
-      listOfTurtles.add(
-    }
+    Card stepForward = new Card("stepForward", "Blue");
+    Card turnLeft = new Card("turnLeft", "Yellow");
+    Card turnRight = new Card("turnRight", "Purple");
+    card bug = new Card("bug", "Brown");
+    cardDeck[0] = stepForward;
+    cardDeck[0] = turnLeft;
+    cardDeck[0] = turnRight;
+    cardDeck[0] = bug;
   }
 }
+
