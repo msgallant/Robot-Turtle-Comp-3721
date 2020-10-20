88d7a0aebcf7ac068f56c6a8f0ed0ddcffad62d3
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-20 19:20:11 -0300

    Update GameBoardModel.java

--- GameBoardModel.java
+++ GameBoardModel.java
@@ -1,1 +1,30 @@
-
+public class GameBoardModel
+{
+  private int numPlayers;
+  private String message;
+  private ArrayList<RobotTurtle> listOfTurtles = new ArrayList<RobotTurtle>();
+  public GameBoardModel(int players)
+  {
+    numPlayers = players;
+    
+  }
+  public static void createRobotTurtles()
+  {
+    if (numPlayers == 1)
+    {
+      listOfTurtles.add(
+    }
+    else if (numPlayers == 2)
+    {
+      listOfTurtles.add(
+    }
+    else if (numPlayers == 3)
+    {
+      listOfTurtles.add(
+    }
+    else
+    {
+      listOfTurtles.add(
+    }
+  }
+}
