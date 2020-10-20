9af5ed0481ae8fd8aeb36966017ecc0958eeebbe
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-20 18:34:35 -0300

    Update RobotTurtle.java

--- RobotTurtle.java
+++ RobotTurtle.java
@@ -1,5 +1,7 @@
 //Marcia Gallant and Loryn Losier
-private int yPos, xPos, direction;
+
+//for direction 1 represents north, 2 represents east, 3 represents south, 4 represents west
+private int yPos, xPos, direction; 
 private String name, colour;
 
 public class RobotTurtle(int x, int y, int d, String n, String c)
