1444690e29fb17d27fa9dfd78798035ffcde4302
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-20 19:54:18 -0300

    Create RobotJewel.java

---
+++ RobotJewel.java
@@ -0,0 +1,13 @@
+
+public class RobotJewel
+{
+  private int xPos, yPos;
+  private String colour;
+  public RobotJewel(int x, int y, String c)
+  {
+    xPos = x;
+    yPos = y;
+    colour = c;
+  }
+  
+}
