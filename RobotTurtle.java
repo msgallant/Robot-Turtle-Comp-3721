d212f1244225febb9c7542033cb860604390b3e5
LorynLosier <43613465+LorynLosier@users.noreply.github.com>
2020-10-20 18:28:17 -0300

    Update RobotTurtle.java

--- RobotTurtle.java
+++ RobotTurtle.java
@@ -1,3 +1,7 @@
-//Marcia Gallant and Loryn Losier
+/*Comp 3721 - Object-Oriented Design and Methodology
+* Milestone 3
+* Due October 27th, 2020
+* Marcia Gallant and Loryn Losier
+*/
 
 public class RobotTurtle
