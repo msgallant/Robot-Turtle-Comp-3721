3d8778367cde8c89444ceab9bcb5d341c4aeba75
LorynLosier <43613465+LorynLosier@users.noreply.github.com>
2020-10-22 21:27:10 -0300

    Update DisplayGameBoard.java

--- DisplayGameBoard.java
+++ DisplayGameBoard.java
@@ -1,7 +1,37 @@
-public class DisplayGameBoard
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DisplayGameBoard extends JFrame
 {
-  public DisplayGameBoard ()
-  {
-  
-  }
+ JButton T[][] = new JButton[8][8];
+ 
+ public DisplayGameBoard(String title)
+ {
+	 
+	super (title);
+	setLayout(new GridLayout(8, 8, 1 ,1));
+	setDefaultCloseOperation(EXIT_ON_CLOSE);  
+	int a=1;
+	for(int i=0; i<8; i++)
+	{
+		for( int j=0; j<8; j++)
+		{	
+			T[i][j] = new JButton();
+			T[i][j].setIcon(new ImageIcon("src/Images/GameBoard"+ a +".jpg"));
+			a++;
+			add(T[i][j]);
+		}
+	}
+ }
+ 
+ public static void main(String[] args)
+ {
+	 DisplayGameBoard rt = new DisplayGameBoard("Robot Turtles");
+	 rt.setVisible(true);
+	 rt.setSize(500, 500);
+ }
+ 
 }
