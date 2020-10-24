117eb4265dfd6403141173cf3a11e4b84a39c245
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-24 18:48:35 -0300

    Update Crate.java

--- Crate.java
+++ Crate.java
@@ -5,60 +5,51 @@ public class Crate extends Tile
 	//private ArrayList<String> movableTileTypes = new ArrayList<String>();
 	//private String tileType1 = "Normal";
 	//need to check for all tile types that crate can be pushed onto
+
 	
 	public Crate(int x, int y) 
 	{
 		super(x, y);
 		
 		this.setTileType("Crate");
-		findAndSetMovableDirections();
+		//findAndSetMovableDirections(); //need to do it in gameboard class not here?
 	}
 	
 	public void findAndSetMovableDirections()
 	{
-		if (this.getNTile() != null)
+		if (this.getNTile()!=null)
 		{
-			if (this.getNTile().getTileType() != "Normal" || this.getNTile().getOccupied() == true)
+			
+			if (!this.getNTile().getTileType().contentEquals("Normal") || this.getNTile().getOccupied() == true)
 			{
 				this.setNMovable(false);
 			}
-			else
-			{
-				this.setNMovable(true);
-			}
+
 		}
 		if (this.getSTile() != null)
 		{
-			if (this.getSTile().getTileType() != "Normal"  || this.getSTile().getOccupied() == true)
+			if (!this.getSTile().getTileType().contentEquals("Normal")  || this.getSTile().getOccupied() == true)
 			{
 				this.setSMovable(false);
 			}
-			else
-			{
-				this.setSMovable(true);
-			}
+			
+
 		}
 		if (this.getWTile() != null)
 		{
-			if (this.getWTile().getTileType() != "Normal"  || this.getWTile().getOccupied() == true)
+			if (!this.getWTile().getTileType().equals("Normal")  || this.getWTile().getOccupied() == true)
 			{
 				this.setWMovable(false);
 			}
-			else
-			{
-				this.setWMovable(true);
-			}
+
 		}
 		if (this.getETile() != null)
 		{
-			if (this.getETile().getTileType() != "Normal"  || this.getETile().getOccupied() == true)
+			if (!this.getETile().getTileType().contentEquals("Normal") || this.getETile().getOccupied() == true)
 			{
 				this.setEMovable(false);
 			}
-			else
-			{
-				this.setEMovable(true);
-			}
+
 		}
 		
 		
