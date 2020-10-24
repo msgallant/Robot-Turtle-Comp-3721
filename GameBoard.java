a87c9cc935ab7795cfdcf3152e05f531426f7019
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-24 18:52:59 -0300

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,48 @@
-
 public class GameBoard
 {
-  
+	private Tile[][] tileList;
+	
+	public GameBoard()
+	{
+		tileList = new Tile[8][8];
+		
+	}
+	
+	//x and y are position on gameboard, valid coordinates are 0-7
+	public Tile[][] addStoneWall(int x, int y)
+	{
+		StoneWall t = new StoneWall(x, y);
+		tileList[x][y] =t;
+		return tileList;
+	}
+	
+	public Tile[][] addCrate(int x, int y)
+	{
+		Crate t = new Crate(x, y);
+		tileList[x][y] =t;
+		return tileList;
+	}
+	public Tile[][] addJewel(int[] pos, String color)
+	{
+
+		RobotJewel r = new RobotJewel(pos[0], pos[1], color);
+		tileList[pos[0]][pos[1]] = r;
+		return tileList;
+	}
+	public Tile[][] addTurtle(int[] pos)
+	{
+
+		tileList[pos[0]][pos[1]].setOccupied(true);
+		return tileList;
+	}
+	
+	//need move turtle the updates occupied status
+	public Tile[][] updateTurtlePositionOnBoard(int[] newPos, int[] oldPos)
+	{
+		tileList[oldPos[0]][oldPos[1]].setOccupied(false);
+		tileList[newPos[0]][newPos[1]].setOccupied(true);
+		return tileList;
+	}
+	
+
 }
