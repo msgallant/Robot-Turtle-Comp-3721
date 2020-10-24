d43f11b13dc6c3c606d8e2382744b8b8ec112f7e
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-24 18:49:46 -0300

    Update RobotTurtle.java

--- RobotTurtle.java
+++ RobotTurtle.java
@@ -4,10 +4,11 @@
 * Marcia Gallant and Loryn Losier
 */
 //for direction 1 represents north, 2 represents east, 3 represents south, 4 represents west
-public class RobotTurtle
+public class RobotTurtle 
 {
   private int yPos, xPos, direction; 
   private String name, colour;
+  // direction: 1=north, 2=east, 3=south, 4 =west
 
   public RobotTurtle(int x, int y, int d, String n, String c)
   {
@@ -19,5 +20,103 @@ public class RobotTurtle
     
   
   }
+  
+  public void setXPos(int x)
+  {
+	  xPos = x;
+  }
+  public void setYPos(int y)
+  {
+	  yPos = y;
+  }
+  public void setDirection(int d)
+  {
+	  direction = d;
+  }
 
+  public int getXPos()
+  {
+	  return xPos;
+  }
+  public int getYPos()
+  {
+	  return yPos;
+  }
+  public int getDirection()
+  {
+	  return direction;
+  }
+  public String getName()
+  {
+	  return name;
+  }
+  public String getColour()
+  {
+	  return colour;
+  }
+  
+  //corects x and y position coordinates after moving forward, this method is only called if turtle moves forward!
+  public void updateTurtlePosition()
+  {
+	  if (direction == 1)
+	  {
+		  yPos -= 1;
+	  }
+	  else if (direction == 2)
+	  {
+		  xPos += 1;
+	  }
+	  else if (direction == 3)
+	  {
+		  yPos += 1;
+	  }
+	  else if (direction == 4)
+	  {
+		  xPos -= 1;
+	  }
+  }
+
+  
+  //corects direction value
+  public void directionShiftRight()
+  {
+	  if (direction == 1)
+	  {
+		  direction = 2;
+	  }
+	  else if (direction == 2)
+	  {
+		  direction = 3;
+	  }
+	  else if (direction == 3)
+	  {
+		  direction = 4;
+	  }
+	  else if (direction == 4)
+	  {
+		  direction = 1;
+	  }
+  }
+  
+  //corects direction value
+  public void directionShiftLeft()
+  {
+	  if (direction == 1)
+	  {
+		  direction = 4;
+	  }
+	  else if (direction == 2)
+	  {
+		  direction = 1;
+	  }
+	  else if (direction == 3)
+	  {
+		  direction = 2;
+	  }
+	  else if (direction == 4)
+	  {
+		  direction = 3;
+	  }
+  }
+  
 }
