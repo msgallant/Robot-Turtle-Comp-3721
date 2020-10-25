dc9f0579a3bc949310ddfcaeb30dd9e98beeb28e
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-25 15:51:54 -0300

    Update GameBoardModel.java

--- GameBoardModel.java
+++ GameBoardModel.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class GameBoardModel
 {
-  private static int numPlayers;
+  private static int numPlayers = 1; //default
   private static int numCards = 4;
   private static int[] JEWEL_ONE_POSITION = {4, 3};
   private static int[] JEWEL_TWO_POSITION = {3, 4};
@@ -49,25 +49,25 @@ public class GameBoardModel
       //int x, int y, int d, String n, String c
       RobotTurtle t = new RobotTurtle(TURTLE_ONE_POSITION[0],TURTLE_ONE_POSITION[1],TURTLE_ONE_POSITION[2],TURTLE_ONE_NAME, TURTLE_ONE_COLOUR);
       listOfTurtles.add(t);
-      gameBoard.addTurtle(TURTLE_ONE_POSITION);
+      gameBoard.addRobotTurtle(TURTLE_ONE_POSITION);
     }
     if (numPlayers >= 2)
     {
     	RobotTurtle t = new RobotTurtle(TURTLE_TWO_POSITION[0],TURTLE_TWO_POSITION[1],TURTLE_TWO_POSITION[2],TURTLE_TWO_NAME, TURTLE_TWO_COLOUR);
         listOfTurtles.add(t);
-        gameBoard.addTurtle(TURTLE_TWO_POSITION);
+        gameBoard.addRobotTurtle(TURTLE_TWO_POSITION);
     }
     if (numPlayers >= 3)
     {
     	RobotTurtle t = new RobotTurtle(TURTLE_THREE_POSITION[0],TURTLE_THREE_POSITION[1],TURTLE_THREE_POSITION[2],TURTLE_THREE_NAME, TURTLE_THREE_COLOUR);
         listOfTurtles.add(t);
-        gameBoard.addTurtle(TURTLE_THREE_POSITION);
+        gameBoard.addRobotTurtle(TURTLE_THREE_POSITION);
     }
     if (numPlayers >= 4)
     {
     	RobotTurtle t = new RobotTurtle(TURTLE_FOUR_POSITION[0],TURTLE_FOUR_POSITION[1],TURTLE_FOUR_POSITION[2],TURTLE_FOUR_NAME, TURTLE_FOUR_COLOUR);
         listOfTurtles.add(t);
-        gameBoard.addTurtle(TURTLE_FOUR_POSITION);
+        gameBoard.addRobotTurtle(TURTLE_FOUR_POSITION);
     }
 
   }
@@ -77,8 +77,8 @@ public class GameBoardModel
     Card turnLeft = new Card("Turn Left", "Yellow");
     Card turnRight = new Card("Turn Right", "Purple");
     Card bug = new Card("Bug", "Brown");
-    cardDeck[0] = stepForward;
-    cardDeck[1] = turnLeft;
+    cardDeck[0] = turnLeft;
+    cardDeck[1] = stepForward;
     cardDeck[2] = turnRight;
     cardDeck[3] = bug;
   }
@@ -88,20 +88,20 @@ public class GameBoardModel
      if (numPlayers >= 1)
     {
       //int x, int y, String c
-      gameBoard.addJewel(JEWEL_ONE_POSITION, JEWEL_ONE_COLOUR);
+      gameBoard.addRobotJewel(JEWEL_ONE_POSITION, JEWEL_ONE_COLOUR);
 
     }
     if (numPlayers >= 2)
     {
-    	gameBoard.addJewel(JEWEL_TWO_POSITION, JEWEL_TWO_COLOUR);
+    	gameBoard.addRobotJewel(JEWEL_TWO_POSITION, JEWEL_TWO_COLOUR);
     }
     if (numPlayers >= 3)
     {
-    	gameBoard.addJewel(JEWEL_THREE_POSITION, JEWEL_THREE_COLOUR);
+    	gameBoard.addRobotJewel(JEWEL_THREE_POSITION, JEWEL_THREE_COLOUR);
     }
     if (numPlayers >= 4)
     {
-    	gameBoard.addJewel(JEWEL_FOUR_POSITION, JEWEL_FOUR_COLOUR);
+    	gameBoard.addRobotJewel(JEWEL_FOUR_POSITION, JEWEL_FOUR_COLOUR);
     }
     
   }
@@ -111,33 +111,38 @@ public class GameBoardModel
 	  //need to pass to moveTurtle in turtle class and gameboard
 	  //need to check if able to move
 	  int[] oldPos = {rt.getXPos(), rt.getYPos()};
-	  previousPosition = oldPos; //stores position in case of bug card
-	  previousCard = c;
+	  if (!c.equals(cardDeck[3]))//if it's not the bug card
+	  {
+		  previousPosition = oldPos; //stores position in case of bug card
+		  previousCard = c;
+	  }
 	  //updates the turtles position in the object itself
-	  if (c.equals(cardDeck[0])) //if card = step forward
+	  if (c.equals(cardDeck[1])) //if card = step forward
 	  {
 		  rt.updateTurtlePosition(); //update turtle x, y coordinates depending on what direction the turtle is facing
 		  int[] newPos = {rt.getXPos(), rt.getYPos()}; 
 		  gameBoard.updateTurtlePositionOnBoard(newPos, oldPos); //update gameboard that new position is occupied with a turtle and old position is not
 	  }
-	  else if (c.equals(cardDeck[1]))// turn left card
+	  else if (c.equals(cardDeck[0]))// turn left card
 	  {
 		  rt.directionShiftLeft();
 	  }
 	  else if (c.equals(cardDeck[2])) //turn right card
 	  {
+		  
 		  rt.directionShiftRight();
 	  }
 	  else // the bug card, //need to update turtle position in object + update gameboard
 	  {
-		  if (previousCard.equals(cardDeck[0])) //moves forward card
+		  System.out.println("entered else" + " prev card: " + previousCard.getCardType());
+		  if (previousCard.equals(cardDeck[1])) //moves forward card
 		  {
 			  rt.setXPos(previousPosition[0]);
 			  rt.setYPos(previousPosition[1]);
 			  
 			  gameBoard.updateTurtlePositionOnBoard(previousPosition, oldPos);
 		  }
-		  else if (previousCard.equals(cardDeck[1]))// turn left card
+		  else if (previousCard.equals(cardDeck[0]))// turn left card
 		  {
 			  rt.directionShiftRight();//turn right into original position
 		  }
@@ -151,6 +156,17 @@ public class GameBoardModel
 	  
 	  
   }
-  
+  public ArrayList<RobotTurtle> getListOfRobotTurtles()
+  {
+	  return listOfTurtles;
+  }
+  public Card[] getDeckOfCards()
+  {
+	  return cardDeck;
+  }
+  public GameBoard getGameBoard()
+  {
+	  return gameBoard;
+  }
 }
 
