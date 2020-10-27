bb5151c418ba64d771505ea6fde78a3a9f9659cd
msgallant <71039715+msgallant@users.noreply.github.com>
2020-10-26 23:25:14 -0300

    Add files via upload

---
+++ TestGBController.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGBController {
+
+	
+	public static void main(String[] args)
+	{
+		GBController gbc = new GBController();
+		gbc.findNumPlayers();
+	
+		
+	}
+
+}
