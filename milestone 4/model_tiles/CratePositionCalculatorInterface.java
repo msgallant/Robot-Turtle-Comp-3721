cd3fa36eb5799fe6e2203c0c78ecd57df165dc07
msgallant <71039715+msgallant@users.noreply.github.com>
2020-11-10 15:57:19 -0400

    Delete CratePositionCalculatorInterface.java

--- milestone 4/model_tiles/CratePositionCalculatorInterface.java
+++
@@ -1,6 +0,0 @@
-package model_tiles;
-
-public interface CratePositionCalculatorInterface {
-	public boolean[] findMovableDirectionsForCrates(Tile t);
-	public int[] findNewCratePosition(int[] oldPos, int directionPushed);
-}
