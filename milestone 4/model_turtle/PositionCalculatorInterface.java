3db42d3f506c7d97754bac3f7a83ba20b6c0ed1f
msgallant <71039715+msgallant@users.noreply.github.com>
2020-11-10 15:56:25 -0400

    Delete PositionCalculatorInterface.java

--- milestone 4/model_turtle/PositionCalculatorInterface.java
+++
@@ -1,6 +0,0 @@
-package model_turtle;
-
-public interface PositionCalculatorInterface extends RobotTurtlePositionCalculatorInterface{
-	boolean checkIfTileAheadExists();
-	int[] calculateNewPosition();
-}
